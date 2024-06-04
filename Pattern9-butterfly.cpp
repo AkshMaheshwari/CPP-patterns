d65ac761723dc91f87bae0ee74a2c2a83df2170d
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-06-04 14:40:18 +0530

    Create Pattern9-butterfly.cpp

---
+++ Pattern9-butterfly.cpp
@@ -0,0 +1,44 @@
+//Butterfly pattern 
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout << "Enter number of rows: ";
+  cin >> n;
+  for(int i=1;i<=n;i++)//upper half of the butterfly
+    {
+      for(int j=1;j<=i;j++)
+        {
+          cout << "*";
+        }
+      
+      for(int j=1;j<=2*n-2*i;j++)
+        {
+          cout << " ";
+        }
+      for(int j=1;j<=i;j++)
+        {
+          cout << "*";
+        }
+      cout <<endl;
+    }
+  for(int i=n-1;i>=1;i--)//lower half of the butterfly
+    {
+      for(int j=1;j<=i;j++)
+        {
+          cout << "*";
+        }
+
+      for(int j=1;j<=2*n-2*i;j++)
+        {
+          cout << " ";
+        }
+      for(int j=1;j<=i;j++)
+        {
+          cout << "*";
+        }
+      cout <<endl;
+    }
+  
+}
